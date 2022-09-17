0a0b4037ead1dc5cab978ce70f7a01a2fee28ecc
Benjamin Rome <benjamin.rome10@gmail.com>
2022-09-17 15:31:15 -0400

    more setup

---
+++ segment.py
@@ -0,0 +1,12 @@
+# Code written by Benjamin Rome and Luke Williams under the supervision of Dr. Charles V. Stewart
+# in the Computer Vision Laboratory at Rensselaer Polytechnic Institute.
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import sys
+
+
+if __name__ == "__main__":
+    img = cv2.imread("./images/MVZ_285_egg.jpg")
+
+
