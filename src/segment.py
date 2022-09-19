3019369c52e23a07c2e04e3fbedb279d54578d35
Luke Williams <williams.luke.2001@gmail.com>
2022-09-19 18:51:26 -0400

    fixing contour function

--- src/segment.py
+++ src/segment.py
@@ -1,30 +1,35 @@
 import numpy as np
 import cv2
 
-from typing import List
+from typing import List, Tuple
 
 
-def egg_contours(img: np.ndarray, canny_thresh: int = 100, arc_thresh: int = 100) -> List[np.ndarray]:
+def egg_contours(
+        img: np.ndarray,
+        dp_thresh: Tuple[int, int] = (8, 23),
+        area_thresh: int = 500) -> List[np.ndarray]:
     """
     Find contours of egg in image
     :param img: Image to find contours in (RGB)
-    :param canny_thresh: Threshold for canny edge detection
-    :param arc_thresh: Threshold for arc length of contours
+    :param dp_thresh: Threshold for number of curves (low, high)
+    :param area_thresh: Threshold for area of contour
     :return: List of egg contours
     """
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
     # Blur image
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-
-    # Apply canny edge detection
-    edges = cv2.Canny(blur, canny_thresh, canny_thresh * 2)
+    ret, thresh = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)
 
     # Find contours
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # filter for circular contours
-    contours = [c for c in contours if cv2.arcLength(c, True) > arc_thresh]
+    # filter for ovular contours
+    contour_list = []
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+        area = cv2.contourArea(contour)
+        if (len(approx) > dp_thresh[0]) & (len(approx) < dp_thresh[1]) & (area > area_thresh):
+            contour_list.append(contour)
 
-    return contours
+    return contour_list
