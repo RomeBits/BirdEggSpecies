3bc6375525510025332bdff7e3c2f6266275ae51
Luke Williams <williams.luke.2001@gmail.com>
2022-09-26 11:51:34 -0400

    added util to load all images in the dataset

--- src/load.py
+++ src/load.py
@@ -1,9 +1,10 @@
 from urllib.request import urlopen
 import numpy as np
 import cv2
+from typing import List
 
 
-def load_image(url: str):
+def load_image(url: str) -> np.ndarray:
     """
     Load image from url
     :param url: url of image
@@ -13,3 +14,22 @@ def load_image(url: str):
     image = image.read()
     image = cv2.imdecode(np.asarray(bytearray(image), dtype="uint8"), cv2.IMREAD_COLOR)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def load_images(urls: list) -> List[np.ndarray]:
+    """
+    Load images from urls
+    :param urls: list of urls
+    :return: list of images
+    """
+    return [load_image(url) for url in urls]
+
+def load_images_from_url_csv(path: str) -> List[np.ndarray]:
+    """
+    Load images from csv file
+    :param path: path to csv file
+    :return: list of images
+    :rtype: List[np.ndarray]
+    """
+    with open(path, 'r') as f:
+        urls = [x.split(',')[1] for x in f.read().splitlines() if len(x) > 0]
+    return load_images(urls)
