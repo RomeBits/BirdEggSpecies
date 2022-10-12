82c2a5d8c644c90da185c9e9b1f7efcbec6d5b86
Luke Williams <williams.luke.2001@gmail.com>
2022-10-12 12:10:34 -0400

    create rectangle utility

--- src/fit_ellipses.py
+++ src/fit_ellipses.py
@@ -14,4 +14,18 @@ def fit_ellipses(contours: np.ndarray, num_eggs: int) -> np.ndarray:
         ellipse = cv2.fitEllipse(contours[i])
         ellipses.append(ellipse)
 
-    return ellipses
+    return np.asarray(ellipses)
+
+def fit_rectangles(contours: np.ndarray, num_eggs: int) -> np.ndarray:
+    """
+    Fit rectangle to contour
+    :param num_eggs: number of eggs
+    :param contours: Contours to fit rectangles to
+    :return: Rectangle
+    """
+    rectangles = []
+    for i in range(num_eggs):
+        rectangle = cv2.minAreaRect(contours[i])
+        rectangles.append(rectangle)
+
+    return np.asarray(rectangles)
