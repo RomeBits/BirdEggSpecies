3df85e04a8d63c92e2a105a009c9abdf447b57b0
Luke Williams <williams.luke.2001@gmail.com>
2022-10-24 18:46:27 -0400

    basic CNN model

--- src/models.py
+++ src/models.py
@@ -1,37 +1,22 @@
-from torchvision import models
 import torch.nn as nn
 import torch
 
-class ResNet18Embedding(models.ResNet):
-    def __init__(self, embedding_dim=128):
-        super().__init__(models.resnet.BasicBlock, [2, 2, 2, 2])
-        self.fc = nn.Linear(512, embedding_dim)
-        self.embedding_dim = embedding_dim
+class CNN(nn.Module):
+    def __init__(self, num_classes=2, num_channels=3, kernel_size=3, stride=1, padding=1):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 16, kernel_size, stride, padding)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size, stride, padding)
+        self.fc1 = nn.Linear(32 * 16 * 16, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-        return x
-
-
-class CNNClassifier(nn.Module):
-    def __init__(self, input_dim, embedding_dim, num_classes):
-        super().__init__()
-        self.embedding = ResNet18Embedding(embedding_dim)
-        self.classifier = nn.Linear(embedding_dim, num_classes)
-
-    def forward(self, x):
-        x = self.embedding.forward(x)
-        x = self.classifier(x)
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(-1, 32 * 16 * 16)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
